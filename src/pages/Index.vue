06ec031c2447ba54d558610fce85a8ec815ddf19
angelito borinaga <borinaga.angelito@gmail.com>
2020-10-03 12:27:38 +0800

    update

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,17 +3,19 @@
     <div class="col q-pt-md q-px-md">
       <q-input
         v-model="search"
-        @keyup.enter="getWeatherBySearch"
+        @keyup.enter="getWeatherBySearchErrorHandle"
         placeholder="Search"
         dark
         borderless
+        ref="searchLocation"
+        :error="[val => !!val || 'Field is required']"
       >
         <template v-slot:before>
-          <q-icon @click="getLocation" name="my_location" />
+          <q-icon @click="getWeatherByCoords" name="my_location" />
         </template>
 
         <template v-slot:append>
-          <q-btn @click="getWeatherBySearch" round dense flat icon="search" />
+          <q-btn @click="getLocation" round dense flat icon="search" />
         </template>
       </q-input>
     </div>
@@ -28,7 +30,14 @@
         </div>
         <div class="text-h1 text-weight-thin q-my-lg relative-position">
           <span>{{ Math.round(weatherData.main.temp) }}</span>
-          <span class="text-h4 relative-position degree">&deg;C</span>
+          <span class="text-h4 relative-position degree">&deg;C </span>
+        </div>
+
+        <div id="info">
+          <i class="fas fa-sun"></i> {{ sunrise }} <i class="far fa-sun"></i>
+          {{ sunset }} <i class="fas fa-tint"></i> {{ humidity }}
+          <i class="far fa-sun"></i> {{ pressure }} <i class="fas fa-wind"></i>
+          {{ wind }}
         </div>
       </div>
 
@@ -63,12 +72,19 @@ export default {
   name: "PageIndex",
   data() {
     return {
+      minTemp: "",
+      maxTemp: "",
+      sunrise: "",
+      sunset: "",
+      pressure: "",
+      humidity: "",
+      wind: "",
       search: "",
       weatherData: null,
       lat: null,
       lon: null,
       apiUrl: "https://api.openweathermap.org/data/2.5/weather",
-      apiKey: ""
+      apiKey: "83c8782a5f9b96efe9de7559c9deb360"
     };
   },
   computed: {
@@ -97,18 +113,76 @@ export default {
       this.$axios(
         `${this.apiUrl}?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}&units=metric`
       ).then(response => {
+        this.pressure = response.data.main.pressure;
+        this.humidity = response.data.main.humidity + "%";
+        this.wind = response.data.wind.speed + "m/s";
+        this.overcast = response.data.weather[0].description;
+        this.icon =
+          "images/" + response.data.weather[0].icon.slice(0, 2) + ".svg";
+        this.sunrise = new Date(
+          response.data.sys.sunrise * 1000
+        ).toLocaleTimeString("en-PH", {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: true
+        });
+        this.sunset = new Date(
+          response.data.sys.sunset * 1000
+        ).toLocaleTimeString("en-PH", {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: true
+        });
         this.weatherData = response.data;
         this.$q.loading.hide();
       });
+    },
+
+    getWeatherBySearchErrorHandle() {
+      this.$refs.searchLocation.validate();
+      if (!this.$refs.searchLocation.hasError) {
+        this.getWeatherBySearch();
+      }
     },
     getWeatherBySearch() {
       this.$q.loading.show();
       this.$axios(
         `${this.apiUrl}?q=${this.search}&appid=${this.apiKey}&units=metric`
-      ).then(response => {
-        this.weatherData = response.data;
-        this.$q.loading.hide();
-      });
+      )
+        .then(response => {
+          this.pressure = response.data.main.pressure;
+          this.humidity = response.data.main.humidity + "%";
+          this.wind = response.data.wind.speed + "m/s";
+          this.overcast = response.data.weather[0].description;
+          this.icon =
+            "images/" + response.data.weather[0].icon.slice(0, 2) + ".svg";
+          this.sunrise = new Date(
+            response.data.sys.sunrise * 1000
+          ).toLocaleTimeString("en-PH", {
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: true
+          });
+          this.sunset = new Date(
+            response.data.sys.sunset * 1000
+          ).toLocaleTimeString("en-PH", {
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: true
+          });
+          this.weatherData = response.data;
+          this.$q.loading.hide();
+        })
+        .catch(error => {
+          if (error.message) {
+            this.$q.dialog({
+              dark: true,
+              title: "Alert",
+              message: "Location Not Found"
+            });
+            this.$q.loading.hide();
+          }
+        });
     }
   }
 };
@@ -135,5 +209,23 @@ export default {
   background: url(../../public/skyline.png)
   background-size: contain;
   background-position: center bottom
+}
+
+.q-field__messages{
+  font-size: 15px
+}
+
+.icon {
+  position: inherit;
+  top: 2px;
+  padding-left: 8px;
+}
+
+#temp-values {
+  text-align: right;
+  text-justify: distribute;
+  display: block;
+  position: relative;
+  top: -60px;
 }
 </style>
